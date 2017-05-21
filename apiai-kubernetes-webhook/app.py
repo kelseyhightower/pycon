88cbc379a771db03d82bdeda811917a3ed61592b
Kelsey Hightower <kelsey.hightower@gmail.com>
2017-05-21 05:56:00 -0700

    clean up apiai-kubernetes-webhook

--- apiai-kubernetes-webhook/app.py
+++ apiai-kubernetes-webhook/app.py
@@ -1,49 +1,64 @@
-from __future__ import division
-from pprint import pprint
-from googleapiclient import discovery
-from oauth2client.service_account import ServiceAccountCredentials
-from flask import Flask
-from flask import make_response
-from flask import request, Response
-from functools import wraps
+# Copyright 2017 Google Inc. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+#
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
 import json
+from __future__ import division
+from functools import wraps
+from pprint import pprint
+
 import kubernetes.client
 import kubernetes.config
 from kubernetes.client.rest import ApiException
 
+from googleapiclient import discovery
+from oauth2client.service_account import ServiceAccountCredentials
+
+from flask import Flask
+from flask import make_response
+from flask import request, Response
+
 app = Flask(__name__)
 
-project_id = 'hightowerlabs'
-zone = 'us-central1-c'
-cluster_id = 'pycon'
+basic_auth_username = os.getenv("BASIC_AUTH_USERNAME", "")
+basic_auth_password = os.getenv("BASIC_AUTH_PASSWORD", "")
 
+namespace = os.getenv("NAMESPACE", "default")
+
+cluster_id = os.getenv("CLUSTER_ID", "")
+project_id = os.getenv("PROJECT_ID", "")
+zone = os.getenv("ZONE", "us-central1-c")
 scopes = [
-    'https://www.googleapis.com/auth/compute.readonly',
-    'https://www.googleapis.com/auth/cloud-platform'
+    "https://www.googleapis.com/auth/compute.readonly",
+    "https://www.googleapis.com/auth/cloud-platform"
 ]
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name('/etc/apiai-kubernetes-webhook/service-account.json', scopes)
+credentials = ServiceAccountCredentials.from_json_keyfile_name("/etc/apiai-kubernetes-webhook/service-account.json", scopes)
 
-compute_service = discovery.build('compute', 'v1', credentials=credentials)
-container_service = discovery.build('container', 'v1', credentials=credentials)
+compute_service = discovery.build("compute", "v1", credentials=credentials)
+container_service = discovery.build("container", "v1", credentials=credentials)
 
 kubernetes.config.load_incluster_config()
-api_instance = kubernetes.client.AppsV1beta1Api()
-namespace = 'default'
+kubernetes_service = kubernetes.client.AppsV1beta1Api()
 
 
 def check_auth(username, password):
-    """This function is called to check if a username /
-    password combination is valid.
-    """
-    return username == '' and password == ''
+    """This function is called to check if a username password combination is valid."""
+    return username == basic_auth_username and password == basic_auth_password
 
 def authenticate():
     """Sends a 401 response that enables basic auth"""
-    return Response(
-    'Could not verify your access level for that URL.\n'
-    'You have to login with proper credentials', 401,
-    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+    return Response("Login required", 401, {"WWW-Authenticate": 'Basic realm="Login Required"'})
 
 def requires_auth(f):
     @wraps(f)
@@ -54,66 +69,70 @@ def requires_auth(f):
         return f(*args, **kwargs)
     return decorated
 
-@app.route('/webhook', methods=['POST'])
+
+@app.route("/webhook", methods=["POST"])
 @requires_auth
 def webhook():
     req = request.get_json(silent=True, force=True)
 
     action = req.get("result").get("action")
+    parameters = req.get("result").get("parameters")
 
     if action == "cluster_status":
         return format_response(get_cluster_status())
-
-    parameters = req.get("result").get("parameters")
-
     if action == "create_deployment":
        return format_response(create_deployment(parameters))
-
     if action == "scale_deployment":
        return format_response(scale_deployment(parameters))
-
     if action == "update_deployment":
        return format_response(update_deployment(parameters))
+
 
 def format_response(res):
     res = json.dumps(res, indent=4)
     r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
+    r.headers["Content-Type"] = "application/json"
     return r
 
+
 def create_deployment(parameters):
+    image_name = parameters["image_name"]
+    image_tag = parameters["image_tag"]
+
     body = kubernetes.client.AppsV1beta1Deployment()
     body.metadata = {
-        'name': parameters['image_name'],
-        'labels': {
-            'app': parameters['image_name'] 
+        "name": image_name,
+        "labels": {
+            "app": image_name 
         }
     }
 
     body.spec = {
-        'template': {
-           'metadata': {
-               'labels': {
-                 'app': parameters['image_name']
+        "template": {
+           "metadata": {
+               "labels": {
+                 "app": image_name
                }
            },
-           'spec': {
-               'containers': [
+           "spec": {
+               "containers": [
                    {
-                      'name': parameters['image_name'],
-                      'image': "gcr.io/hightowerlabs/{}:{}".format(parameters['image_name'], parameters['image_tag'])
+                      "name": image_name,
+                      "image": "gcr.io/{}/{}:{}".format(project_id, image_name, image_tag)
                    }
                ]
            }
         }
     }
 
+    speech = ""
+
     try:
-        api_response = api_instance.create_namespaced_deployment(namespace, body)
+        response = kubernetes_service.create_namespaced_deployment(namespace, body)
+        speech = "Deploying {} {} into the {} cluster.".format(image_name, image_tag, cluster_id)
     except ApiException as e:
         print("Exception when calling AppsV1beta1Api->create_namespaced_deployment: %s\n" % e)
-
-    speech = "Deploying {} {} into the pycon cluster.".format(parameters['image_name'], parameters['image_tag'])
+        speech = "There was an error while creating the {} deployment".format(deployment_name)
 
     return {
         "speech": speech,
@@ -121,19 +140,20 @@ def create_deployment(parameters):
         "source": "kubernetes-webhook"
     }
 
+
 def scale_deployment(parameters):
-    deployment_name = parameters['deployment_name']
-    replica_count = int(parameters['replica_count'])
+    deployment_name = parameters["deployment_name"]
+    replica_count = int(parameters["replica_count"])
 
     body = kubernetes.client.AppsV1beta1Deployment()
     body.spec = {
-       'replicas': replica_count
+       "replicas": replica_count
     }
 
     speech = ""
 
     try:
-        api_response = api_instance.patch_namespaced_deployment(deployment_name, namespace, body)
+        response = kubernetes_service.patch_namespaced_deployment(deployment_name, namespace, body)
         speech = "Scaling the {} deployment to {}".format(deployment_name, replica_count)
     except ApiException as e:
         print("Exception when calling AppsV1beta1Api->patch_namespaced_deployment: %s\n" % e)
@@ -147,17 +167,18 @@ def scale_deployment(parameters):
 
 
 def update_deployment(parameters):
-    deployment_name = parameters['deployment_name']
-    image_tag = parameters['image_tag']
+    deployment_name = parameters["deployment_name"]
+    image_name = deployment_name
+    image_tag = parameters["image_tag"]
 
     body = kubernetes.client.AppsV1beta1Deployment()
     body.spec = {
-        'template': {
-           'spec': {
-               'containers': [
+        "template": {
+           "spec": {
+               "containers": [
                    {
-                      'name': deployment_name,
-                      'image': "gcr.io/hightowerlabs/{}:{}".format(deployment_name, image_tag)
+                      "name": image_name,
+                      "image": "gcr.io/{}/{}:{}".format(project_id, image_name, image_tag)
                    }
                ]
            }
@@ -167,7 +188,7 @@ def update_deployment(parameters):
     speech = ""
 
     try:
-        api_response = api_instance.patch_namespaced_deployment(deployment_name, namespace, body)
+        response = kubernetes_service.patch_namespaced_deployment(deployment_name, namespace, body)
         speech = "Updating the {} deployment to version {}".format(deployment_name, image_tag)
     except ApiException as e:
         print("Exception when calling AppsV1beta1Api->patch_namespaced_deployment: %s\n" % e)
@@ -184,18 +205,18 @@ def get_cluster_status():
     cluster_request = container_service.projects().zones().clusters().get(projectId=project_id, zone=zone, clusterId=cluster_id)
     cluster_response = cluster_request.execute()
 
-    num_nodes = cluster_response['currentNodeCount']
-    machine_type = cluster_response['nodeConfig']['machineType']
+    num_nodes = cluster_response["currentNodeCount"]
+    machine_type = cluster_response["nodeConfig"]["machineType"]
 
     machine_type_request = compute_service.machineTypes().get(project=project_id, zone=zone, machineType=machine_type)
     machine_type_response = machine_type_request.execute()
-    num_cpus = machine_type_response['guestCpus']
-    memory_in_mb = machine_type_response['memoryMb']
+    num_cpus = machine_type_response["guestCpus"]
+    memory_in_mb = machine_type_response["memoryMb"]
 
     total_cpus = num_nodes * num_cpus
     total_memory_in_gb = ((num_nodes * memory_in_mb) / 1024)
 
-    speech = "The {} cluster is running with {} CPUs and {} of ram.".format(cluster_id, total_cpus, total_memory_in_gb)
+    speech = "The {} cluster is running with {} CPUs and {} gigs of ram.".format(cluster_id, total_cpus, total_memory_in_gb)
 
     return {
         "speech": speech,
@@ -203,5 +224,6 @@ def get_cluster_status():
         "source": "kubernetes-webhook"
     }
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host="0.0.0.0", port=8080)
